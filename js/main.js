0e55178d8c3997b769e12080716ac0fdf5ddc792
Martin S <m.schauer@gmx.at>
2019-09-28 17:15:03 +0200

    Improved the display on mobile; Added functionality for the back-button; Added a language direction switcher; Fixed some smaller issues.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let test_words = [];
+let text_word_index = 0;
 var selected_language = 'en';
+let lang_first = 'de';
 let words_set = [];
 let translation_visible = 0;
 for (let lesson_count = 0; lesson_count < lessons.length; lesson_count++) {
@@ -59,71 +61,46 @@ lesson_selection_content += '</div>';
 
 $('.lesson-selection').append(lesson_selection_content);
 $('.lesson-mass-selection-checkbox').change(function() {
-  $(this).nextAll('.lesson-mass-selection-container').first().children('.lesson-selection-checkbox').attr('checked', $(this).is(':checked'));
+  $(this).nextAll('.lesson-mass-selection-container').first().children('.lesson-selection-checkbox').prop('checked', $(this).is(':checked'));
 });
-
-/*var lesson_selection_content = '';
-for (var word_count = 0; word_count < kanjis.length; word_count++) {
- if (word_count % 10 == 0) {
-   if (word_count % 100 == 0) {
-     if (lesson_selection_content.length > 0) {
-       lesson_selection_content += '</div>';
-     }
-     lesson_selection_content += '<br /><input type="checkbox" class="lesson-mass-selection-checkbox" value="mass-' + word_count + '" /><span class="lesson-mass-selection-label">Kanji ' + (word_count + 1) + ' - ' + (((word_count + 100) < kanjis.length) ? (word_count + 100) : kanjis.length)  + '</span><div class="lesson-mass-selection-container">';
-   }
-   lesson_selection_content += '<input type="checkbox" class="lesson-selection-checkbox" value="' + word_count + '" />Kanji ' + (word_count + 1) + ' - ' + (((word_count + 10) < kanjis.length) ? (word_count + 10) : kanjis.length) + '<span class="learn-words"></span><br />';
- }
-}
-lesson_selection_content += '</div>';
-$('.lesson-selection').append(lesson_selection_content);
-
-$('.lesson-mass-selection-checkbox').change(function() {
-  $(this).nextAll('.lesson-mass-selection-container').first().children('.lesson-selection-checkbox').attr('checked', $(this).is(':checked'));
-});*/
  
 $('#start-test-button').click(function() {
   var selected_boxes = $('.lesson-selection-checkbox:checked');
+  // Use all lessons in case none has been selected.
   if (selected_boxes.length === 0) {
-    alert('Please select at least one lesson first.');
+    selected_boxes = $('.lesson-selection-checkbox');
   }
+  
   // Start the test.
-  else {
-    let selected_words = [];
-    selected_boxes.each(function() {
-      let start_id = parseInt($(this).val());
-      selected_words = selected_words.concat(lessons[start_id].vocabulary);
-    });
-    test_words = shuffle(selected_words);
+  let selected_words = [];
+  selected_boxes.each(function() {
+    let start_id = parseInt($(this).val());
+    selected_words = selected_words.concat(lessons[start_id].vocabulary);
+  });
+  test_words = shuffle(selected_words);
+  text_word_index = 0;
 
-    // Clear the history.
-    // $('.kanji-history-list').empty();
+  // Update the Word-counts.
+  $('.remaining-word-max, .remaining-word-count').text(test_words.length);
 
-    // Update the Kanji-counts.
-    $('.remaining-word-max, .remaining-word-count').text(test_words.length);
-
-    // Unset the mistake count.
-    /*mistake_count = 0;
-    $('.remaining-word-mistakes-count').text(mistake_count);*/
-
-    // Display the first item.
-    setTranslationVisible(0);
-    $('#word-display-container-de .word-display-content').html(test_words[0].de.replace(/%u/g, '&#x'));
-    $('#word-display-container-jp .word-display-content').html(test_words[0].jp.replace(/%u/g, '&#x'));
-    $('.word-display-container, .word-navigation').show();
-  }
+  // Display the first item.
+  setTranslationVisible(0);
+  $('#word-display-container-de .word-display-content').html(test_words[0].de.replace(/%u/g, '&#x'));
+  $('#word-display-container-jp .word-display-content').html(test_words[0].jp.replace(/%u/g, '&#x'));
+  $('.word-display-container, .word-navigation').show();
 });
 
+// Next word / display translation.
 $('#next-word-button').click(function() {
   if (translation_visible) {
-    test_words.shift();
-    $('.remaining-word-count').text(test_words.length);
-
-    if (test_words.length > 0) {
-      $('#word-display-container-de .word-display-content').html(test_words[0].de.replace(/%u/g, '&#x'));
-      $('#word-display-container-jp .word-display-content').html(test_words[0].jp.replace(/%u/g, '&#x'));
+    if ((text_word_index + 1) < test_words.length) {
+      text_word_index++;
+      $('.remaining-word-count').text(test_words.length - text_word_index);
+      $('#word-display-container-de .word-display-content').html(test_words[text_word_index].de.replace(/%u/g, '&#x'));
+      $('#word-display-container-jp .word-display-content').html(test_words[text_word_index].jp.replace(/%u/g, '&#x'));
     }
     else {
-      alert('Test done!');
+      $('.remaining-word-count').text(0);
       return;
     }
   }
@@ -131,9 +108,21 @@ $('#next-word-button').click(function() {
   setTranslationVisible(!translation_visible);
 });
 
+// Previous word.
+$('#prev-word-button').click(function() {
+  setTranslationVisible(0);
+
+  if (text_word_index > 0) {
+    text_word_index--;
+    $('#word-display-container-de .word-display-content').html(test_words[text_word_index].de.replace(/%u/g, '&#x'));
+    $('#word-display-container-jp .word-display-content').html(test_words[text_word_index].jp.replace(/%u/g, '&#x'));
+    $('.remaining-word-count').text(test_words.length - text_word_index);
+  }
+});
+
 let setTranslationVisible = function(visible) {
   translation_visible = visible;
-  $('#word-display-container-jp .word-display-content').toggle(!!visible);
+  $('#word-display-container-' + (lang_first === 'de' ? 'jp' : 'de') + ' .word-display-content').toggle(!!visible);
 };
 
 $('.lesson-selection-toggle').click(function() {
@@ -141,90 +130,11 @@ $('.lesson-selection-toggle').click(function() {
   $(this).text($(this).text() === 'Open lesson selection' ? 'Close lesson selection' : 'Open lesson selection')
 });
 
-/*$("#answer-textbox").on('keyup', function (e) {
-   if (e.keyCode == 13 && test_words.length > 0) {
-     var answer = $(this).val();
-     $(this).val('');
-
-     var old_kanji = test_words.shift();
-
-     var answer_trimmed = $.trim(answer.toLowerCase());
-     var history_item_class = '';
-     var history_item_suffix = '';
-     var answer_correct = ($.inArray(answer_trimmed, getAllowedValues(old_kanji.meaning)) !== -1 || $.inArray(answer_trimmed, getAllowedValues(unescape(old_kanji.kun.length > 0 ? old_kanji.kun : old_kanji.on))) !== -1);
-     // Correct answer.
-     if (answer_correct) {
-       $('.remaining-word-count').text(test_words.length);
-       history_item_class = 'kanji-history-item-correct';
-     }
-     // Wrong answer.
-     else {
-       history_item_class = 'kanji-history-item-wrong';
-       // Add the wrong kanji again and shuffle the list.
-       test_words.push(old_kanji);
-       test_words = shuffle(test_words);
-
-       // Check for which kanji the user mistook the current one.
-       for (var word_count = 0; word_count < words_set.length; word_count++) {
-         if ($.inArray(answer_trimmed, getAllowedValues(words_set[word_count].meaning)) !== -1 || $.inArray(answer_trimmed, getAllowedValues(unescape(words_set[word_count].kun.length > 0 ? words_set[word_count].kun : words_set[word_count].on))) !== -1) {
-           history_item_suffix = '<br /><span class="kanji-history-item-mistake">(mistaken for ' + words_set[word_count].kanji.replace('%u', '&#x') + '; - ' + words_set[word_count].meaning + ' &rarr; ' + (words_set[word_count].kun.length > 0 ? words_set[word_count].kun.replace(/%u/g, '&#x') : '<span class="kanji-on">' + words_set[word_count].on.replace(/%u/g, '&#x') + '</span>') + ')</span>';
-           break;
-         }
-       }
-
-       // Update the mistake count.
-       mistake_count++;
-       $('.remaining-word-mistakes-count').text(mistake_count);
-     }
-     $('#answer-status').attr('class', history_item_class.replace('kanji-history-item-', 'answer-'));
-
-     // Create a new item for the history.
-     var new_item = '<div class="kanji-history-item ' + history_item_class + '">' + old_kanji.kanji.replace('%u', '&#x') + '; - ' + old_kanji.meaning + ' &rarr; ' + (old_kanji.kun.length > 0 ? old_kanji.kun.replace(/%u/g, '&#x') : '<span class="kanji-on">' + old_kanji.on.replace(/%u/g, '&#x') + '</span>') + history_item_suffix + '</div>';
-
-     // Add the history item to the start of the list.
-     var history_items = $('#kanji-history-list-all .kanji-history-item');
-     if (history_items.length >= 10) {
-       history_items.last().remove();
-     }
-     $('#kanji-history-list-all').prepend(new_item);
-
-     // On errors also add it to the error list.
-     if (!answer_correct) {
-       history_items = $('#kanji-history-list-errors .kanji-history-item');
-       if (history_items.length >= 10) {
-         history_items.last().remove();
-       }
-       $('#kanji-history-list-errors').prepend(new_item);
-     }
-
-     if (test_words.length > 0) {
-       $('.word-display').html(test_words[0].kanji.replace('%u', '&#x') + ';');
-     }
-     else {
-       alert('Test done!');
-     }
-   }
-});
-
-$('.language-switcher-lang').click(function() {
- if (!$(this).hasClass('active')) {
-   $('.language-switcher-lang').toggleClass('active');
-   selected_language = $(this).data('lang');
-   if (selected_language === 'en') {
-     words_set = kanjis.map(a => Object.assign({}, a));
-   }
-   // German.
-   else {
-     words_set = kanjis_de.map(a => Object.assign({}, a));
-   }
- }
-});*/
-
 $('.learn-words').click(function() {
  var learn_overlay = $('#learn-overlay');
  var lesson_id = parseInt($(this).prev().val());
 
- // Clear the table and add the new kanjis to the table.
+ // Clear the table and add the new words to the table.
  var table_body = $('#learn-overlay-table tbody');
  let vocabulary = lessons[lesson_id].vocabulary;
  table_body.empty();
@@ -291,23 +201,40 @@ search_ac._renderItem = function( table, item ) {
     .appendTo( table );
 };
 
-/*$('.history-area-tabs').tabs({
-  'classes': {
-    "ui-tabs": "ui-corner-all",
-    "ui-tabs-nav": "ui-corner-all",
-    "ui-tabs-tab": "ui-corner-all",
-    "ui-tabs-panel": "ui-corner-all"
-  }
-});*/
-
 // Hide the overlay on click.
 $('body').delegate('#learn-overlay', 'click', function() {
   $(this).hide();
 });
 
+$('.direction-selection').click(function() {
+  // Show the current translation.
+  setTranslationVisible(1);
+  
+  let dir_select = $('.lang-direction');
+  if (lang_first === 'de') {
+    lang_first = 'jp';
+    dir_select.html('&larr;');
+    $('.word-display').append($('#word-display-container-de'));
+  }
+  else {
+    lang_first = 'de';
+    dir_select.html('&rarr;');
+    $('.word-display').append($('#word-display-container-jp'));
+  }
+  
+  $('.remaining-word-count').text(test_words.length - text_word_index);
+  
+  // Hide the new translation.
+  setTranslationVisible(0);
+});
+
+// React on button presses.
 document.onkeydown = function(e) {
   switch (e.keyCode) {
     case 37:
+      if (test_words.length > 0) {
+        $('#prev-word-button').click();
+      }
       break;
     case 39:
       if (test_words.length > 0) {
@@ -317,6 +244,7 @@ document.onkeydown = function(e) {
   }
 };
 
+// React on Swipe.
 $(document).touchwipe({
   wipeLeft: function() {
     if (test_words.length > 0) {
@@ -324,6 +252,9 @@ $(document).touchwipe({
     }
   },
   wipeRight: function() {
+    if (test_words.length > 0) {
+      $('#prev-word-button').click();
+    }
   },
   wipeUp: function() { },
   wipeDown: function() { },
